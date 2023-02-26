e74ffd9b623e8cc6601d8039dfd926d6941ed21d
olechka27 <olechka.dobrova@gmail.com>
2023-02-26 18:39:10 +0500

    Initial commit

---
+++ school-homeWork_15.02/src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.hogwarts.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
